12bd5e6654d445f7876cd16bbbbe1c569917a051
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-23 23:25:09 -0400

    add basic HTML with UI form. assign input to variables

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business Logic
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+// User Interface Logic
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+  console.log("number 1: " + number1)
+  console.log("number 2: " + number2)
+  console.log("Operator: " + operator)
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
