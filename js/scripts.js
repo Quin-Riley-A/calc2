d1c6cf60cfc77059f044b01f59068135717f4ff0
Quin-Riley-A <quin.asselin1@gmail.com>
2022-08-23 23:33:03 -0400

    add branching based on operator and display result

--- js/scripts.js
+++ js/scripts.js
@@ -21,9 +21,17 @@ function handleCalculation(event) {
   const number1 = parseInt(document.querySelector("input#input1").value);
   const number2 = parseInt(document.querySelector("input#input2").value);
   const operator = document.querySelector("input[name='operator']:checked").value;
-  console.log("number 1: " + number1)
-  console.log("number 2: " + number2)
-  console.log("Operator: " + operator)
+  let result;
+  if (operator ==="add") {
+    result = add(number1, number2);
+  } else if (operator ==="subtract") {
+    result = subtract(number1,number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1,number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2)
+  }
+  document.getElementByID("output").innerText = "Your answer is: " + result;
 }
 
 window.addEventListener("load", function() {
